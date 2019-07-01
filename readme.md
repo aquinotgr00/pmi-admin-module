fce45cc0674e1d56fba25eaa0459af9c49127f50
zuhdil <zuhdil@gmail.com>
2019-07-02 00:28:18 +0700

    Laravel package boilerplate.

---
+++ readme.md
@@ -0,0 +1,14 @@
+# pmi-admin
+
+## Install
+`composer require bajaklautmalaka/pmi-admin`
+
+## Usage
+Write a few lines about the usage of this package.
+
+## Testing
+Run the tests with:
+
+``` bash
+vendor/bin/phpunit
+```
---
+++ src/PmiAdminServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BajakLautMalaka\PmiAdmin;
+
+use Illuminate\Support\ServiceProvider;
+
+class PmiAdminServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
