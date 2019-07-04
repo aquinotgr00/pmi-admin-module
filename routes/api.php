a598ebff7976629868a98aa8663424cdfe7f5191
Paulus <lonewolfffffff@gmail.com>
2019-07-05 00:05:44 +0700

    - route middleware : force model binding - User controller : update show() and complete update()

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,17 @@
 <?php
-    Route::post('admin/login', 'AuthController@login');
-    Route::get('admin/forgot_password','UserController@passwordReset');
+    Route::prefix('admin')->group(function() {
+        Route::post('login', 'AuthController@login');
+        Route::get('forgot_password','UserController@passwordReset');
+    });
     
-    Route::middleware('auth:admin')->group(function() {
-        Route::get('admin/logout', 'AuthController@logout');
-    
-        Route::apiResource('admin','UserController');
-        Route::put('admin/{id}/{status}','UserController@statusUpdate');
+    Route::middleware(['auth:admin','bindings'])->group(function() {
         Route::put('admin/password','UserController@passwordUpdate');
+        Route::get('admin/logout', 'AuthController@logout');
+        Route::put('admin/{user}/{status}','UserController@statusUpdate');
+        
+        Route::apiResource('admin','UserController');
         
     });
-            
     
     
     
